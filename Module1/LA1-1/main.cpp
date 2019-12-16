01f70a76a6b10ec8106d4ec454467e670da1758f
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 10:06:07 -0700

    My age command

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+int main()
+{
+    int age;    //int: signed integers
+    // cout: for system output
+
+    
+    
+    std::cout << "Now inside VSCode and github" << std::endl;
+    cout << "Another line uder namespace std" << endl;
+    cout << " wildcats" << "" << "WSU" <<"Ogden" << endl;
+    // cin: for system/user input
+    cout << "What is your age?" << endl;
+    cin >> age;
+    cout << "I see you are: " << age << endl;
+
+    cout << "Integers are: " << sizeof(int) << " bytes." << endl;
+
+
     return 0;
 }
