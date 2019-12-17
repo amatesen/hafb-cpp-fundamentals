2b8ca8f43d32c5525e9b8970986a69151cc9b845
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-17 14:03:31 -0700

    Tuesday

--- Module1/TO1-5/src/main.cpp
+++ Module1/TO1-5/src/main.cpp
@@ -10,7 +10,9 @@
 int main() 
 {
     std::cout << "The lamborghini's horse power: " << lamborghini::HorsePower() << std::endl;
+    lamborghini :: cout();
     std::cout << "The porsche's horse power: " << porsche::HorsePower() << std::endl;
+    //porsche :: cout();
     std::cout << "A regular car's horse power: " << ::HorsePower() << std::endl;
     return 0;
 }
