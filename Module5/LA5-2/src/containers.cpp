a8e4375e602aea3fd7c7c00e20ca0e0bc4578f9b
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-19 16:27:11 -0700

    Thursday

--- Module5/LA5-2/src/containers.cpp
+++ Module5/LA5-2/src/containers.cpp
@@ -3,3 +3,22 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <string>
+
+void Load_Data(std::vector<Data> &data, const std::string &input_file)
+{
+    csvstream csvinput(inputfile);   //Open file
+  // // Rows have a key = column name, value = cell data
+    std::map<std::string, std::string> row;
+
+  // // Extract the animal column
+    while (csvinput >> row)
+  {
+    std::cout << row["ID"] << "," << row["name"] << "," << row["animal"] std::endl;
+    temp.id = row["id"];
+    temp.id = row["name"];
+    temp.id = row["animal"];
+    data.push_back(temp); // Load Data into vector
+
+  }
+}
--- Module5/LA5-2/src/my_sets.cpp
+++ Module5/LA5-2/src/my_sets.cpp
@@ -1,7 +1,50 @@
+/**
+ * @file my_sets.cpp
+ * @author your name (you@domain.com)
+ * @brief Learn about sets:
+ *  Elements are sorted according to the values
+ *  Elements are are all distinct value
+ * @version 0.1
+ * @date 2019-12-19
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include <iostream>
-#include <set>
+#include <set>  // A container with a UNIQUE group SORTED elements
+// Methods: size(), empty(), insert(), erase(), 
+// find(): returns the position of the first element equal to the provided value
+//      you should always compare the value agaist the end() of the container
 
 int main()
 {
+    std::set<int> numbers;
+    numbers.insert(10);
+    numbers.insert(20);
+    numbers.insert(30);
+    // Search for a value
+    if(numbers.find(10) != numbers.end())   // end() is NOT the last element, but then end of the set byte
+    {
+        std::cout << "10 is in numbers" << std::endl;
+    }
+    std::cout<< "The set has: " << numbers.size() << " elements" << std::endl;
+    // Iterate over the set
+    std::cout << "Using iterator" << std::endl;
+    for(auto it = std::begin(numbers); it != std::end(numbers); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+    // Range based
+    std::cout<<"Range based"<< std::endl;
+    for(const auto& value: numbers)
+    {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
 
+
+
+
+    return 0;
 }
